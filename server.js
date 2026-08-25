47007b9c75f36bea08b8f1477ea9ce1cb1977fc6
Shazia Jabeen <2shaziajabeen005@gmail.com>
2026-08-25 15:32:46 +0500

    Update backend

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://todo-app-brown-one-94.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
 app.use(express.json());
+// Handle preflight requests explicitly
+app.options("*", cors());
 
 // Routes
 app.use("/api/todos", todoRoutes);
